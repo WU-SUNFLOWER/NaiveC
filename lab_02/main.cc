d796b545759e08db7ff548fe61d9c83370513901
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2025-01-29 23:23:25 +0800

    Add cpplint check tool to the project

--- lab_02/main.cc
+++ lab_02/main.cc
@@ -1,7 +1,3 @@
-// Copyright 2024 WU-SUNFLOWER. All rights reserved.
-// Use of this source code is governed by a GPL-style license that can be
-// found in the LICENSE file.
-
 #include <iostream>
 
 #include "llvm/IR/Module.h"
